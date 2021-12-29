71aeb6d433939f954c8d52a48da9ff1a6c1ef263
Step_ri69@itstepru.onmicrosoft.com <Step_ri69@itstepru.onmicrosoft.com>
2021-12-29 20:11:15 +0200

    Добавьте файлы проекта.

---
+++ Model/Roster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userControl.Model
+{
+    public class Roster
+    {
+        public string TeamName { get; private set; }
+        private readonly List<Player> players = new List<Player>();
+        public IEnumerable<Player> Players
+        {
+            get => new List<Player>(players);
+        }
+
+        public Roster(string teamName, 
+            IEnumerable<Player> _players)
+        {
+            TeamName = teamName;
+            this.players.AddRange(_players);
+        }
+    }
+}
